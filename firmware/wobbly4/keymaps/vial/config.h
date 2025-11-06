483a8ef437655e4154510fef6b264772bf7151f2
Valerie Tsoi <valerietsoi@Valeries-MacBook-Pro.local>
2025-11-06 13:46:25 +0000

    repo name check

---
+++ firmware/wobbly4/keymaps/vial/config.h
@@ -0,0 +1,37 @@
+// Copyright 2025 valerie tsoi (@valerietsoi)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+//vial
+#define VIAL_KEYBOARD_UID {0xDB, 0x0D, 0x8E, 0xBB, 0x36, 0x12, 0x43, 0xDC}
+
+#define DYNAMIC_KEYMAP_MACRO_COUNT 48
+#define VIAL_COMBO_ENTRIES 10
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+//#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+//#define VIAL_UNLOCK_COMBO_COLS {2, 2}
+
+//#define BOOTMAGIC_ROW_RIGHT {0,0}
+//#define BOOTMAGIC_COLUMN_RIGHT {0,3}
+
+
+//#define IGNORE_MOD_TAP_INTERRUPT
+//#define PERMISSIVE_HOLD
+
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
